fdbe558e0fd90946fe58522908cffc7760af6ac0
Balram Awasthi <balram.awasthi@visionhealth.co.uk>
2023-11-07 16:28:06 +0530

    Added program files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome in Programs");
+//SwappingNumbers swappingNumbers = new SwappingNumbers();
+//swappingNumbers.WithoutVariable();
+//Console.WriteLine("Write you own Power without using multiplication(*) and division(/) operators");
+//Power power = new Power();
+//int powerout = power.Poweralculation(5,2);
+//Console.WriteLine($"Power " + powerout);
+
+A obj = new B();
+obj.Print();
